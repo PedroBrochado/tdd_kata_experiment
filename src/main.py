3c514b63fa1b502ee642c532b35bc3c6cba1bf55
Pedro Brochado <pedro.brochado@gousto.co.uk>
2021-03-12 12:03:23 +0000

    support \n in the string

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 
 def add(number_string):
     string_list = number_string.split(",")
+    string_list = [x.split("\n") for x in string_list]
+
+    return sum(sum_list_of_strings(x) for x in string_list)
+
+
+def sum_list_of_strings(string_list):
     int_list = [int(element) for element in string_list if element != ""]
     return sum(int_list)
+
+
+
--- tests/test_add.py
+++ tests/test_add.py
@@ -10,7 +10,7 @@ def test_calculate_single_number_string_returns_sum():
 
 
 def test_calculate_two_number_string_returns_sum():
-    assert add("1,2") == 3
+    assert add("1,2,3,4,5") == 15
 
-
-
+def test_calculate_number_string_with_newline_separator():
+    assert add("1\n2,3") == 6
